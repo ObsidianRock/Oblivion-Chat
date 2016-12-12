61fbb7fac93aa239e82683e81a0685cd4ac901be
Obsidian <ObsidianRock@users.noreply.github.com>
2016-12-12 21:37:05 +0000

    initial tinker

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 
 import rethinkdb as r
 from flask import Flask, render_template
-
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret KEY SHHH!'
+
+socketio = SocketIO(app)
+
+
+@socketio.on('message')
+def handle_message(msg):
+    send(msg, broadcast=True)
+    print('received message: ' + msg)
 
 
 @app.route('/')
@@ -13,4 +22,6 @@ def main():
 
 if __name__ == '__main__':
 
-    app.run(debug=True)
+    socketio.run(app, debug=True)
+
+
