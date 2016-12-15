e671b517eb3582585d6d47a11c6d274e6f95fd84
Obsidian <ObsidianRock@users.noreply.github.com>
2016-12-15 14:16:43 +0000

    some further experimentation

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-
-import rethinkdb as r
 from database import Message
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret KEY SHHH!'
@@ -11,16 +9,19 @@ app.config['SECRET_KEY'] = 'secret KEY SHHH!'
 socketio = SocketIO(app)
 
 
-@socketio.on('message')
-def handle_message(msg):
-    message = Message()
-    message.commit(msg)
-    send(msg, broadcast=True) #takes whatever message coming in and send to everyone connected
-
-
 @app.route('/')
 def main():
+
     return render_template('home.html')
+
+
+@socketio.on('chat_message')
+def handle_message(msg):
+    message = Message()
+    print(msg['message'])
+    message.commit(msg['message'])
+    emit('chat_response', {'message': msg['message']}, broadcast=True)  # takes whatever message coming in and send to everyone connected
+
 
 
 if __name__ == '__main__':
