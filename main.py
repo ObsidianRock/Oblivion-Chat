d57faa875098c7df0a7495832acd25b5e126b180
Obsidian <ObsidianRock@users.noreply.github.com>
2016-12-15 17:36:32 +0000

    added a connection class

--- main.py
+++ main.py
@@ -8,24 +8,40 @@ app.config['SECRET_KEY'] = 'secret KEY SHHH!'
 
 socketio = SocketIO(app)
 
+message = Message()
+
+
+class Connection:
+    def __init__(self):
+        self.conn = 0
+
+    def new_connection(self):
+        self.conn += 1
+
+    def left_conn(self):
+        self.conn += -1
+
+    def num_conn(self):
+        return self.conn
+
+connection = Connection()
 
 @app.route('/')
 def main():
-
     return render_template('home.html')
 
 
 @socketio.on('chat_message')
 def handle_message(msg):
-    message = Message()
-    print(msg['message'])
     message.commit(msg['message'])
     emit('chat_response', {'message': msg['message']}, broadcast=True)  # takes whatever message coming in and send to everyone connected
 
 
 @socketio.on('message')
 def handle_connect(msg):
-    send(msg, broadcast=True)
+    connection.new_connection()
+    message_sending = {"message": msg, "connections": connection.num_conn()}
+    emit("new connection", message_sending, broadcast=True)
 
 
 if __name__ == '__main__':
