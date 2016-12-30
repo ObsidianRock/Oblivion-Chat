dbf3a586ba17e0f4c5c70aa30c45a0602b4aa352
Obsidian <ObsidianRock@users.noreply.github.com>
2016-12-30 15:58:32 +0000

    adding sqlalchemy support to chat

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,28 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from setup import DATABASE_URL
 
 socketio = SocketIO()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+db = SQLAlchemy()
+
 
 def create_app(debug=True):
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret KEY SHHH!'
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
     app.debug = debug
 
     login_manager.init_app(app)
     socketio.init_app(app)
     bcrypt.init_app(app)
+    db.init_app(app)
 
     from .main import main as main_blueprint
 
