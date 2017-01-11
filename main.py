ace7f92781af3d00c6159c4806dc381f588f2855
Obsidian <ObsidianRock@users.noreply.github.com>
2017-01-11 17:39:46 +0000

    adding database migration

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from app import create_app, socketio, db
 from app.database import UserModel
 from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = create_app()
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 
 def make_shell_context():
     return dict(app=app, db=db, User=UserModel)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
-
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def run():
