f8a7353827e77d419690d7cd8d53f9d695df4d86
Obsidian <ObsidianRock@users.noreply.github.com>
2017-01-24 14:03:14 +0000

    adding rethinkdb database setup as command

--- main.py
+++ main.py
@@ -3,10 +3,30 @@ from app.database import UserModel, RoomModel
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
+import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError, RqlDriverError
 
 app = create_app()
 manager = Manager(app)
 migrate = Migrate(app, db)
+
+
+rethink_db = 'Chat'
+
+@manager.command
+def rethinkDbSetup():
+
+    connection = r.connect(host='localhost', port=28015)
+
+    try:
+        r.db_create(rethink_db).run(connection)
+        r.db(rethink_db).table_create('Message').run(connection)
+        r.db(rethink_db).table_create('Room').run(connection)
+        print('Database setup completed')
+    except RqlRuntimeError:
+        print('App database already exists')
+    finally:
+        connection.close()
 
 
 def make_shell_context():
